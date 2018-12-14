b9e29e1ee5ce5f51b76ebaaed4f52afec0952326
Darshan <brunodarshan@gmail.com>
2018-12-14 07:20:43 -0300

    porra!

--- App.js
+++ App.js
@@ -1,102 +1,119 @@
 import React from 'react';
-import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, ActivityIndicator, FlatList, Text, TextInput, View, ScrollView, ToastAndroid, Button, Dimensions, Modal} from 'react-native';
+import ActionButton, { ActionButtonItem } from 'react-native-action-button'
+import {deepOrange} from './utils/Colors';
+import moment from 'moment'
 
 import firebase from 'react-native-firebase';
 
 export default class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
+  state = {users: null, contentLoaded: false, newUser: {}, modalIsVisible: false}
 
   async componentDidMount() {
-    // TODO: You: Do firebase things
-    // const { user } = await firebase.auth().signInAnonymously();
-    // console.warn('User -> ', user.toJSON());
+    await firebase.firestore().collection('users').get().then((snapshot) => {
+      let users = []
+      snapshot.forEach((doc) => {
+        let user = { name, number, birthday, valid_until } = doc.data();
+        user.reference = doc.reference;
+        users.push(user);
+      })
+      this.setState({users, contentLoaded: true})
+    })
+  }
 
-    // await firebase.analytics().logEvent('foo', { bar: '123'});
+  RenderList(){
+    return(<FlatList data={this.state.users} renderItem={({item}) => <ItemContent user={item} /> } />)
+  }     
+  
+  async submit(){
+    let { newUser } = this.state;
+    await firebase.firestore().collection('users').add(newUser).then((docRef)=>{
+      ToastAndroid.show(`$newUser.name adicionado com sucesso!`, ToastAndroid.SHORT)
+      setTimeout(() => {
+        this.setState({modalIsVisible: false})
+      }, 500)
+    }).catch((error)=> 
+      ToastAndroid.show(`Falha ao salvar dados de usuário.`, ToastAndroid.SHORT)
+    );
   }
 
   render() {
     return (
-      <ScrollView>
-        <View style={styles.container}>
-          <Image source={require('./assets/ReactNativeFirebase.png')} style={[styles.logo]}/>
-          <Text style={styles.welcome}>
-            Welcome to {'\n'} React Native Firebase
-          </Text>
-          <Text style={styles.instructions}>
-            To get started, edit App.js
-          </Text>
-          {Platform.OS === 'ios' ? (
-            <Text style={styles.instructions}>
-              Press Cmd+R to reload,{'\n'}
-              Cmd+D or shake for dev menu
-            </Text>
-          ) : (
-            <Text style={styles.instructions}>
-              Double tap R on your keyboard to reload,{'\n'}
-              Cmd+M or shake for dev menu
-            </Text>
-          )}
-          <View style={styles.modules}>
-            <Text style={styles.modulesHeader}>The following Firebase modules are pre-installed:</Text>
-            {firebase.admob.nativeModuleExists && <Text style={styles.module}>admob()</Text>}
-            {firebase.analytics.nativeModuleExists && <Text style={styles.module}>analytics()</Text>}
-            {firebase.auth.nativeModuleExists && <Text style={styles.module}>auth()</Text>}
-            {firebase.config.nativeModuleExists && <Text style={styles.module}>config()</Text>}
-            {firebase.crashlytics.nativeModuleExists && <Text style={styles.module}>crashlytics()</Text>}
-            {firebase.database.nativeModuleExists && <Text style={styles.module}>database()</Text>}
-            {firebase.firestore.nativeModuleExists && <Text style={styles.module}>firestore()</Text>}
-            {firebase.functions.nativeModuleExists && <Text style={styles.module}>functions()</Text>}
-            {firebase.iid.nativeModuleExists && <Text style={styles.module}>iid()</Text>}
-            {firebase.invites.nativeModuleExists && <Text style={styles.module}>invites()</Text>}
-            {firebase.links.nativeModuleExists && <Text style={styles.module}>links()</Text>}
-            {firebase.messaging.nativeModuleExists && <Text style={styles.module}>messaging()</Text>}
-            {firebase.notifications.nativeModuleExists && <Text style={styles.module}>notifications()</Text>}
-            {firebase.perf.nativeModuleExists && <Text style={styles.module}>perf()</Text>}
-            {firebase.storage.nativeModuleExists && <Text style={styles.module}>storage()</Text>}
-          </View>
-        </View>
-      </ScrollView>
+      <React.Fragment>
+      <View style={styles.container}>
+        { this.state.users ? this.RenderList() : <OnLoading /> }
+      </View>
+      <Modal visible={this.state.modalIsVisible} >
+        <FormUser newUser={this.state.newUser} submit={this.submit.bind(this)} />
+      </Modal>
+      <ActionButton buttonColor={deepOrange} onPress={() => this.setState((prev) => ({modalIsVisible: !prev.modalIsVisible}))}/>
+      </React.Fragment>
     );
   }
 }
 
+function OnLoading(){
+  return (<ActivityIndicator size="large" color={deepOrange} />)
+}
+
+function ItemContent(props) {
+  return(
+      <View key={props.user.reference} style={{padding: 8, backgroundColor: "#fff", width: Dimensions.get('screen').width - 10 }}>
+        <Text style={{fontWeight: 'bold', fontSize: 16}}>{props.user.name}</Text>
+        <Text style={{fontSize: 10}}>{props.user.number}</Text>
+      </View>
+  )
+}
+
+class FormUser extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = props.newUser
+    this.state.isDateTimePickerVisible = false;
+    this.state.currentDatePickerResource = null;
+  }
+
+  _handleDatePicked(data) {
+    if (this.state.currentDatePickerResource === "birthday") {
+      this.setState({birthday: data});
+    } elseif (this.state.currentDatePickerResource === "valid_until") {
+      this.setState({valid_until: date });
+    }
+  }
+  
+
+  render(){
+    let dateOutput = (value && typeof(value) === Date) ? value : Date.format()
+    return(<View style={{paddingHorizontal: 10, paddingVertical: 20}}>
+        <TextInput onChange={(name) => this.setState({name})} accessibilityHint="Nome" value={this.state.name} />
+        <TextInput onChange={(number) => this.setState({number})} accessibilityHint="Telefone" value={this.state.number} />
+        <TouchableWithoutFeedback onPress={() => this.setState({currentDatePickerResource: "birthday", isDateTimePickerVisible: true })}>
+          <View style={{padding: 4}}>
+             <Text style={{fontWeight: '500'}}> `Nascido em: ${moment().format(dateOutput, 'dd/MM/yyyy')}`</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback onPress={() => this.setState({currentDatePickerResource: "valid_until", isDateTimePickerVisible: true })}>
+          <View style={{padding: 4}}>
+             <Text style={{fontWeight: '500'}}> `Nascido em: ${moment().format(dateOutput, 'dd/MM/yyyy')}`</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <DateTimePicker
+          isVisible={this.state.isDateTimePickerVisible}
+          onConfirm={this._handleDatePicked}
+          onCancel={() => this.setState({isDateTimePickerVisible: false, currentDatePickerResource: null})}
+        />
+        <Button title="Salvar" onPress={() => {this.props.submit(); this.setState({isDateTimePickerVisible: false })} }/>
+    </View>)
+  }
+}
+
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: '100%',
+    height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  logo: {
-    height: 120,
-    marginBottom: 16,
-    marginTop: 64,
-    padding: 10,
-    width: 135,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  modules: {
-    margin: 20,
-  },
-  modulesHeader: {
-    fontSize: 16,
-    marginBottom: 8,
-  },
-  module: {
-    fontSize: 14,
-    marginTop: 4,
-    textAlign: 'center',
-  }
 });
